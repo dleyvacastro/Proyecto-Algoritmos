dd3697dd3174476e5d095bf0ee7cebcea427aff2
Nico Quintero <juan24092002@gmail.com>
2020-10-12 14:06:09 -0500

    Creacion del header
    Contiene struct casilla con indice, coordenada y apuntadores a casillas a
    las que se puede mover y
    Class tablero con tamaño, arreglo dinamico, constructor y destructor

---
+++ interface.hpp
@@ -0,0 +1,44 @@
+#ifndef _INTERFACE_HPP_
+#define _INTERFACE_HPP_
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+struct casilla{
+  //indice en el arreglo
+  int indice;
+  //coordenada (x,y)
+  int x;
+  int y;
+  //turno en que el caballo esta en la casilla
+  int turno;
+
+  //adyacencia
+  //cuadrante arriba a la izquierda
+  casilla *ai;
+  casilla *ad;
+  //cuadrante arriba a la derecha
+  casilla *bi;
+  casilla *bd;
+  //cuadrante abajo a la izquierda
+  casilla *ci;
+  casilla *cd;
+  //cuadrante abajo a la derecha
+  casilla *di;
+  casilla *dd;
+};
+
+class Tablero{
+private:
+  casilla *inicio;
+  int largo;
+  int ancho;
+  void inicializar_casillas();
+public:
+  Tablero(int n, int m);
+  ~Tablero();
+};
+
+
+#endif
